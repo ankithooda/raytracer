683dc114c09643aa911217a3e9fccdb2e0e1743b
Ankit <ankithooda23@gmail.com>
2024-09-24 12:04:51 +0530

    Add testcases for the following vector3 functions
    - multiply
    - divide
    - length_sq
    - length

--- tests/vector3_test.c
+++ tests/vector3_test.c
@@ -2,7 +2,7 @@
 #include <math.h>
 #include "../vector3.h"
 
-#define EPSILON 0.0000000001
+#define EPSILON 0.0000001
 
 int double_eq(double a, double b)
 {
@@ -44,10 +44,54 @@ void test_add()
   assert(double_eq(z(v1), 7.77) == 0);
 }
 
+void test_multiply()
+{
+  vector3 *v1 = create(2.22, 3.0, 6.6);
+
+  multiply(v1, 2.22);
+
+  assert(double_eq(x(v1), 4.9284) == 0);
+  assert(double_eq(y(v1), 6.66) == 0);
+  assert(double_eq(z(v1), 14.652) == 0);
+}
+
+void test_divide()
+{
+  vector3 *v1 = create(2.22, 3.0, 6.6);
+
+  divide(v1, 2.45);
+
+  assert(double_eq(x(v1), 0.9061224) == 0);
+  assert(double_eq(y(v1), 1.2244897) == 0);
+  assert(double_eq(z(v1), 2.6938775) == 0);
+
+}
+
+void test_length_sq()
+{
+  vector3 *v = create(1.2, 2.3, 3.4);
+  double l = length_sq(v);
+
+  assert(double_eq(l, 18.2899999) == 0);
+}
+
+
+void test_length()
+{
+  vector3 *v = create(1.2, 2.3, 3.4);
+  double l = length(v);
+
+  assert(double_eq(l, 4.2766809) == 0);
+}
+
 int main()
 {
   test_create_and_getters();
   test_negate();
   test_add();
+  test_multiply();
+  test_divide();
+  test_length_sq();
+  test_length();
   return 0;
 }
